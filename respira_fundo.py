d343550f7c8139fb60a5ab2fdd6046647b52e173
gabriel.rato@ <gabriel.rato@usp.br>
2019-10-21 22:57:47 -0300

    using gunicorn

--- respira_fundo.py
+++ respira_fundo.py
@@ -1,16 +1,9 @@
-import os
-from flask import Flask, jsonify, request
-
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def nao_entre_em_panico():
-    if request.headers.get('Authorization') == '42':
-        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "Não entre em pânico!"})
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from respira_fundo import app
+
+if __name__ == "__main__":
+    app.run()
